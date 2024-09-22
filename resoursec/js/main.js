3f6d827b11ad9aa8f9cc6a264fcc84d5968c59bf
Neamul-karim <neamulkarim1994@gmail.com>
2024-09-22 08:25:24 +0600

    first commit

---
+++ resoursec/js/main.js
@@ -0,0 +1,38 @@
+
+
+(function ($) {
+
+   // Back to top button
+   $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+});
+$('.back-to-top').click(function () {
+    $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    return false;
+});
+
+
+  // Header carousel
+  $(".header-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    items: 1,
+    dots: true,
+    loop: true,
+    nav : false,
+    navText : [
+        '<i class="bi bi-chevron-left"></i>',
+        '<i class="bi bi-chevron-right"></i>'
+    ]
+});
+
+
+// mixitup   portfolio
+var mixer = mixitup('.work-portfolio');
+
+
+})(jQuery);
