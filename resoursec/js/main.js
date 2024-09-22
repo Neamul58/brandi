f591ff9251756ed573a89a07932cb6f8668ef74f
Neamul-karim <neamulkarim1994@gmail.com>
2024-09-22 08:50:12 +0600

    i have change something

--- resoursec/js/main.js
+++ resoursec/js/main.js
@@ -16,6 +16,13 @@ $('.back-to-top').click(function () {
 });
 
 
+// Facts counter
+$('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 2000
+});
+
+
   // Header carousel
   $(".header-carousel").owlCarousel({
     autoplay: true,
